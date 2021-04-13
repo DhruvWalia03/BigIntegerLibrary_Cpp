9d3bc80c0e02a493302fb2a3231ce58f8faf00a4
DhruvWalia03 <59091612+DhruvWalia03@users.noreply.github.com>
2021-04-13 11:52:39 +0530

    Added Max And Min Functions

--- Relational Operators.cpp
+++ Relational Operators.cpp
@@ -38,3 +38,16 @@ bool BigInteger::operator<(BigInteger b) {
     // this represents the object that called the function and the argument passed was b
 }
 
+BigInteger BigInteger::maximum(BigInteger b) {   
+    // if this big integer is greater or equals B we return this else B
+    if(greater((*this),b) || equals((*this),b))
+        return (*this);
+    return b;
+}
+
+BigInteger BigInteger::minimum(BigInteger b) {
+    // if this big integer is less or equals B we return this else B
+    if(less((*this),b) || equals((*this),b))
+        return (*this);
+    return b;
+}
