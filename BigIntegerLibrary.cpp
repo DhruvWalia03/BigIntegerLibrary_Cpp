d60a35258127d30129d3f062c6e344507d7cf3bf
DhruvWalia03 <59091612+DhruvWalia03@users.noreply.github.com>
2021-04-13 11:51:51 +0530

    Added Max And Min Functions

--- BigIntegerLibrary.cpp
+++ BigIntegerLibrary.cpp
@@ -168,6 +168,20 @@ bool BigInteger::operator<(BigInteger b) {
     // this represents the object that called the function and the argument passed was b
 }
 
+BigInteger BigInteger::maximum(BigInteger b) {   
+    // if this big integer is greater or equals B we return this else B
+    if(greater((*this),b) || equals((*this),b))
+        return (*this);
+    return b;
+}
+
+BigInteger BigInteger::minimum(BigInteger b) {
+    // if this big integer is less or equals B we return this else B
+    if(less((*this),b) || equals((*this),b))
+        return (*this);
+    return b;
+}
+
 /*------------------------ARITHMETIC OPERATORS----------------------------------------------*/
 
 BigInteger BigInteger::operator+(BigInteger b) {
